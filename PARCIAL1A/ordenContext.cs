2e055a18aade19b392b22a5a6ef5421b9d34f691
Andrés Enrique Lemus Deleón <andres.lemus@catolica.edu.sv>
2022-03-12 17:46:41 -0600

    Modificación de appsettings.json para establecer el Connection  Stringa la Base de Datos. Se agregó la clase de contexto ordenContext.cs. Se instalaron los paquetes NuGet necesarios. Modificación del archivo Startup.cs para usar el contexto del ConnectionString.

---
+++ PARCIAL1A/ordenContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+//using PARCIAL1A.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARCIAL1A
+{
+    public class ordenContext : DbContext
+    {
+        public ordenContext(DbContextOptions<ordenContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<CompraElementos> CompraElementos { get; set; }
+        //public DbSet<ElementosPorPlato> ElementosPorPlato { get; set; }
+        //public DbSet<PlatosPorCombo> PlatosPorCombo { get; set; }
+        //public DbSet<Platos> Platos { get; set; }
+        //public DbSet<Elementos> Elementos { get; set; }
+    }
+}
